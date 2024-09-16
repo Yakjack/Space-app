9971fffa2a88b0e08e07cdb40afa7667eb661c4b
Jack <h24jacsa@du.se>
2024-09-16 17:55:47 +0200

    Add information about planets

---
+++ Planet_info.py
@@ -0,0 +1,58 @@
+#planetary information page 
+
+## information about the solar system 
+
+##Accept user input and questions about certain planets. 
+
+
+#1. Store Planetary Information in a Dictionary
+
+planets = {
+    "Mercury": {
+        "diameter_km": 4879,
+        "mass_kg": 3.30e23,
+        "distance_from_sun_km": 57.9e6,
+        "atmosphere": "Oxygen, Sodium, Hydrogen, Helium",
+        "moons": 0
+    },
+    "Venus": {
+        "diameter_km": 12104,
+        "mass_kg": 4.87e24,
+        "distance_from_sun_km": 108.2e6,
+        "atmosphere": "Carbon Dioxide, Nitrogen",
+        "moons": 0
+    },
+    "Earth": {
+        "diameter_km": 12742,
+        "mass_kg": 5.97e24,
+        "distance_from_sun_km": 149.6e6,
+        "atmosphere": "Nitrogen, Oxygen",
+        "moons": 1
+    },
+    "Mars": {
+        "diameter_km": 6779,
+        "mass_kg": 6.42e23,
+        "distance_from_sun_km": 227.9e6,
+        "atmosphere": "Carbon Dioxide, Nitrogen, Argon",
+        "moons": 2
+    },
+    # Add other planets like Jupiter, Saturn, Uranus, Neptune here
+}
+##information about the solar system 
+print("there are", {planets[]}, "in the solar system")
+
+#2. 2. Search planet 
+number_of_planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+
+
+#2. 2. Input from the User
+
+def planet_information(planets){
+
+
+
+}
+
+
+#. 3. Display Planet Information
